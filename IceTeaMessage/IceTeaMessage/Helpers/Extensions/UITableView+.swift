ca88bdabce8f8100751556fb1ef9c3dfe51b892b
HoangHai <hoangminhhai1997@gmail.com>
2019-07-09 10:49:35 +0700

    update code

---
+++ IceTeaMessage/IceTeaMessage/Helpers/Extensions/UITableView+.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ProjectBase
+//
+//  Created by Hoàng Hải on 10/16/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func dequeueReusableCell<T>(ofType type: T.Type, for indexPath: IndexPath) -> T? {
+        return self.dequeueReusableCell(withIdentifier: String(describing: type), for: indexPath) as? T
+    }
+    
+}
---
+++ IceTeaMessage/IceTeaMessage/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IceTeaMessage
+//
+//  Created by Hoàng Hải on 7/8/19.
+//  Copyright © 2019 HoangHai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
